08d576a8d1dea92f2bb9901ffb6081d919e4acc5
liuhengren <1016406999@qq.com>
2017-12-19 20:00:08 +0800

    ..

---
+++ lab041/lab4/code/js/autochange.js
@@ -0,0 +1,24 @@
+window.onload=function()
+{
+	var pic=document.getElementById('picScroll');
+	var pic1=document.getElementById('picScroll1');
+	var i;
+	function changeToLeft(){
+		if(pic.scrollLeft>pic1.offsetWidth)
+		{
+			pic.scrollLeft=0;
+		}
+		else
+		{
+	  		pic. scrollLeft++;
+		}
+	}
+	i=setInterval(changeToLeft,10);
+	pic. onmouseover=function(){
+		clearInterval(i);
+	}
+	pic. onmouseout=function(){
+		i=setInterval(changeToLeft,10);
+	}
+}
+
